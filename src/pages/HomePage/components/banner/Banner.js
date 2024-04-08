aa77739fcadb47c0a2cead2daf27f0615f1331d2
MJJUNG33 <jung2674@gmail.com>
2024-04-08 15:01:36 +0900

    Add return for if statement in Banner.js

--- src/pages/HomePage/components/banner/Banner.js
+++ src/pages/HomePage/components/banner/Banner.js
@@ -7,28 +7,30 @@ const Banner = () => {
   const { data, isLoading, error, isError } = usePopularMoviesQuery();
 
   if (isLoading) {
-    <h1>
-      <span class="loader"></span> Loading...
-    </h1>;
+    return (
+      <div>
+        <h1>
+          <span className="loader"></span> Loading...
+        </h1>
+      </div>
+    );
   }
+
   if (isError) {
-    <Alert variant="danger">{error.message}</Alert>;
+    return <Alert variant="danger">{error.message}</Alert>;
   }
 
   return (
     <div
       style={{
-        backgroundImage:
-          "url(" +
-          `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}` +
-          ")",
+        backgroundImage: `url(https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path})`,
       }}
       className="banner"
     >
       <div className="text-white banner-text-area">
         <h1 className="text-white banner-title">{data?.results[0].title}</h1>
         <p className="text-white banner-overview">
-          {data?.results[0].overview}
+          {data?.results[0].overview}{" "}
         </p>
       </div>
     </div>
